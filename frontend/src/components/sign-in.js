475c41b5dc73d3fa45b421076123af5dc1928c60
kodmeron <cameron.b.lloyd@gmail.com>
2022-04-25 00:37:32 +0200

    fromatering

--- frontend/src/components/sign-in.js
+++ frontend/src/components/sign-in.js
@@ -1,29 +1,29 @@
 import React from 'react'
 
 function signIn() {
-  return (
-    <div>
+    return (
+        <div>
 
-    <div className='main-container'>
-    <h2>Användarnamn:</h2>
-    <br/>
-        <input 
-        id='userName' 
-        type="text"
-        placeholder='Användarnamn'
-        />
-        <br/>
-        <h2>Lösernord:</h2>
-        <br/>
-        <input
-            id='userPass'
-            type="text"
-            placeholder="Lösenord"
-        />
-    </div>
-        
-    </div>
-  )
+            <div className='main-container'>
+                <h2>Användarnamn:</h2>
+                <br />
+                <input
+                    id='userName'
+                    type="text"
+                    placeholder='Användarnamn'
+                />
+                <br />
+                <h2>Lösernord:</h2>
+                <br />
+                <input
+                    id='userPass'
+                    type="text"
+                    placeholder="Lösenord"
+                />
+            </div>
+
+        </div>
+    )
 }
 
 export default signIn
