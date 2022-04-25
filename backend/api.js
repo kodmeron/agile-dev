ddfb210fdc6124e1d96d77e5ec156fc1a032f860
kodmeron <cameron.b.lloyd@gmail.com>
2022-04-25 22:06:44 +0200

    cloud database done

--- backend/api.js
+++ backend/api.js
@@ -9,19 +9,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.json())
 app.use(cors());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "..", "build")));
-app.use('/controller',require('./api/controller'));
+app.use('/controller', require('./api/controller'));
 
-mongoose.connect('mongodb://localhost/DatingSite',{useNewUrlParser: true, useUnifiedTopology : true})
-.then(() => console.log('connected'))
-.catch((err)=> console.log(err));
-  
- app.get('*', (req, res) => {
-   res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
- });
+mongoose.connect('mongodb+srv://forever:alone@dating-site.brusq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('connected'))
+  .catch((err) => console.log(err));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+});
 
 
-  app.listen(3001, () => {
-    console.log("server started on port 3001");
-  });
+app.listen(3001, () => {
+  console.log("server started on port 3001");
+});
